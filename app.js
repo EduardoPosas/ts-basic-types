a059bd3783607c5ca626fff825107b4c99470668
Alexander Pozas <a.posas@outlook.com>
2024-01-29 23:58:16 -0600

    ts unknown and never type

--- app.js
+++ app.js
@@ -1,43 +1,33 @@
 /**
- * Function return types & void
- * A function can infer the return type
- * void means that the function does not return anything
+ * Unknown
+ * It accepts any value but with narrowing (checking for the type to assign to it)
  */
-var add = function (n1, n2) { return n1 + n2; };
-function concatenate(t1, t2) {
-    return "".concat(t1, "  ").concat(t2);
+var message;
+var userName;
+message = 5;
+message = 'Max';
+// userName = message; // error, variable with type unknown may be change its type (different to string)
+if (typeof message === 'string') {
+    userName = message; // checked that message is a string
 }
-console.log(add(10, 5));
-console.log(concatenate('hola', 'mundo'));
-// void
-function cartTotal() {
-    var items = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        items[_i] = arguments[_i];
-    }
-    var total = items.reduce(function (previous, current) { return previous + current; }, 0);
-    console.log(total);
-}
-cartTotal(10, 20, 30);
 /**
- * Function as types
- * 1. Function type, it more general
- * 2. Arrow fn notation, we define a type with certain parameters and the returning value
+ * Never
+ * Some fn never return a value
+ * never type represents a value which are never observed, in a return type, it means that the function throws an exception or terminates the execution of the program
  */
-function multiplyTwoNumbers(n1, n2) {
-    return n1 * n2;
+function errorMessage(msg) {
+    throw new Error(message = msg);
 }
-var multiply;
-// let multiply: Function; // this is general, could cause some errors since it could receive diferent functions with diferent parameters, types and functioning
-// multiply = cartTotal // error
-multiply = add;
-console.log(multiply(2, 6));
-// Callbacks
-function addAndPrint(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
+errorMessage('Hubo un error en la petición');
+// It also appears when ts determines there´s nothing else in a union type
+function fn(x) {
+    if (typeof x === "string") {
+        // do something
+    }
+    else if (typeof x === "number") {
+        // do something else
+    }
+    else {
+        x; // has type 'never'!
+    }
 }
-addAndPrint(25, 10, function (result) {
-    console.log(result);
-    return result; // not error but it is unused since the cb return type is void
-});
---
+++ app.ts
@@ -0,0 +1,39 @@
+/** 
+ * Unknown
+ * It accepts any value but with narrowing (checking for the type to assign to it)
+ */
+
+let message: unknown;
+let userName: string;
+
+message = 5;
+message = 'Max';
+
+// userName = message; // error, variable with type unknown may be change its type (different to string)
+if (typeof message === 'string') {
+  userName = message; // checked that message is a string
+}
+
+
+/** 
+ * Never
+ * Some fn never return a value
+ * never type represents a value which are never observed, in a return type, it means that the function throws an exception or terminates the execution of the program
+ */
+
+function errorMessage(msg: string): never {
+  throw new Error(message = msg)
+}
+
+errorMessage('Hubo un error en la petición')
+
+// It also appears when ts determines there´s nothing else in a union type
+function fn(x: string | number) {
+  if (typeof x === "string") {
+    // do something
+  } else if (typeof x === "number") {
+    // do something else
+  } else {
+    x; // has type 'never'!
+  }
+}
